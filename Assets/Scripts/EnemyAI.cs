48a9555320dbb87f222edc4954c4701d20d1c2aa
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2026-04-29 10:35:27 +0530

    feat(script): enemy AI

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Transform player;
+    public float attackRange = 2f;
+    public float damage = 10f;
+    public float attackCooldown = 1f;
+
+    float nextAttack;
+    NavMeshAgent agent;
+    PlayerHealth playerHealth;
+
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        playerHealth = player.GetComponent<PlayerHealth>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        agent.SetDestination(player.position);
+
+        float dist = Vector3.Distance(transform.position, player.position);
+
+        if (dist <= attackRange && Time.time > nextAttack)
+        {
+            playerHealth.TakeDamage(damage);
+            nextAttack = Time.time + attackCooldown;
+        }
     }
 }
