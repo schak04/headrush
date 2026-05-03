9454ab20b561bed769190f563fdc934185145b97
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2026-05-03 23:50:02 +0530

    fix: update Gun script to ignore player collider

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -5,6 +5,7 @@ public class Gun : MonoBehaviour
     public float damage = 25f;
     public float range = 100f;
     public PlayerHealth playerHealth;
+    public LayerMask ignoreLayer;
 
     void Update()
     {
@@ -18,16 +19,18 @@ public class Gun : MonoBehaviour
     {
         RaycastHit hit;
 
-        if (Physics.Raycast(transform.position, transform.forward, out hit, range))
+        if (Physics.Raycast(transform.position, transform.forward, out hit, range, ~ignoreLayer))
         {
             if (hit.collider.CompareTag("Head"))
             {
                 hit.collider.GetComponentInParent<EnemyHealth>().TakeDamage(damage * 2);
                 playerHealth.Heal(10);
+                Debug.Log("HEADSHOT!");
             }
             else if (hit.collider.CompareTag("Enemy"))
             {
                 hit.collider.GetComponent<EnemyHealth>().TakeDamage(damage);
+                Debug.Log("Body Hit");
             }
         }
     }
